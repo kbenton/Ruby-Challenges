288b7e279ea25e1fbe0d90250bde7c2bf16cb639
Kerry Benton <k.benton@gmail.com>
2011-08-10 18:44:29 -0400

    starting over.

--- 3_shortestPath/lib/node.rb
+++ 3_shortestPath/lib/node.rb
@@ -1,13 +1,11 @@
 module ShortestPath
-  class node
+  class Node
     attr_reader :id
-    attr_accessor :source? :current?
 
     def initialize(id)
       @id = id
 
-      @neighbors = Hash.new
-      @neighbors = {}
+      @neighbors = Array.new
     end
   end
 end
